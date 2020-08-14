26200d7a70ec18a6d7b261a66d1d194ee405d3b2
hudsoncliff <hudsonlciffs1999@gmail.com>
2020-08-14 18:39:48 +0900

    項目の追加、管理画面用

--- functions.php
+++ functions.php
@@ -18,70 +18,108 @@ function bs_user_profile_fields( $user ) {
 
     if( current_user_can( 'administrator' ) ) { //スーパー管理者の時だけは変更するためのフォームを出力。それ以下は結果だけを見せる
 
-        if( $edit_user_role == 'subscriber' ) {
+        $bs_customer_status = get_user_meta( $user_id, 'bs-customer-status', true );
 
-            $bs_customer_status = get_user_meta( $user_id, 'bs-customer-status', true );
+        if( !empty( $bs_customer_status ) ) {
 
-            if( !empty( $bs_customer_status ) ) {
+            if( array_key_exists( 'birthday', $bs_customer_status ) ) {
 
-    
-                if( array_key_exists( 'address', $bs_customer_status ) ) {
-    
-                    $user_address = $bs_customer_status['address'];
-                
-                }
-    
-                if( array_key_exists( 'tel', $bs_customer_status ) ) {
-    
-                    $user_tel = $bs_customer_status['tel'];
-                
-                }
-
-                if( array_key_exists( 'agreement', $bs_customer_status ) ) {
-
-                    $user_agreement = $bs_customer_status['agreement'];
-                    $checked_agreement = ( $user_agreement ) ? 'checked="checked"' : null;
-
-                }
+                $user_birthday = $bs_customer_status['birthday'];
 
             }
-            
-            ?>
-            <h2>予約者情報</h2>
-            <p>店舗管理者に通知されるユーザーの情報</p>
-            <table class="form-table">
-                <tr>
-                    <th>
-                        <label for="bs-customer-status[address]">住所</label>
-                    </th>
-                    <td>
-                        <input type="text" name="bs-customer-status[address]" id="bs-customer-status[address]" value="<?php echo $user_address; ?>">
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <label for="bs-customer-status[tel]">電話番号</label>
-                    </th>
-                    <td>
-                        <input type="tel" name="bs-customer-status[tel]" id="bs-customer-status[tel]" value="<?php echo $user_tel; ?>">
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <?php bloginfo( 'name' ); ?>利用規約への同意
-                    </th>
-                    <td>
-                        <input type="checkbox" name="bs-customer-status[agreement]" id="bs-customer-status[agreement]" <?php echo $checked_agreement; ?>>
-                    </td>
-                </tr>
-            </table>
 
-            <?php
+            if( array_key_exists( 'gender', $bs_customer_status ) ) {
+
+                $user_gender = $bs_customer_status['gender'];
+                $checked_male = ( $user_gender == 'male' ) ? ' checked="checked"' : null;
+                $checked_female = ( $user_gender == 'female' ) ? ' checked="checked"' : null;
+
+                $gender_select = '<label>男性
+                <input type="radio" name="bs-customer-status[gender]" id="bs-customer-status[gender]" value="male" ' . $checked_male . '></label>
+                <label>女性
+                <input type="radio" name="bs-customer-status[gender]" id-"bs-customer-status[gender]" value="female" ' . $checked_female . '></label>';
+
+            }
+
+            if( array_key_exists( 'address', $bs_customer_status ) ) {
+
+                $user_address = $bs_customer_status['address'];
+            
+            }
+
+            if( array_key_exists( 'tel', $bs_customer_status ) ) {
+
+                $user_tel = $bs_customer_status['tel'];
+            
+            }
 
         }
+        
+        ?>
+        <h2>予約者情報</h2>
+        <p>袴コンテストように追加した独自項目</p>
+        <table class="form-table">
+            <tr>
+                <th>
+                    <label for="bs-customer-status[birthday]">生年月日</label>
+                </th>
+                <td>
+                    <input type="date" name="bs-customer-status[birthday]" id="bs-customer-status[birthday]" value="<?php echo $user_birthday; ?>">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="bs-customer-status[gender]">性別</label>
+                </th>
+                <td>
+                    <?php echo $gender_select; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="bs-customer-status[address]">住所</label>
+                </th>
+                <td>
+                    <input type="text" name="bs-customer-status[address]" id="bs-customer-status[address]" value="<?php echo $user_address; ?>">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="bs-customer-status[tel]">電話番号</label>
+                </th>
+                <td>
+                    <input type="tel" name="bs-customer-status[tel]" id="bs-customer-status[tel]" value="<?php echo $user_tel; ?>">
+                </td>
+            </tr>
+        </table>
+
+        <?php
 
     } 
     
+}
+
+add_action('personal_options_update', 'bs_user_profile_fields_update', 10, 1);
+add_action('edit_user_profile_update', 'bs_user_profile_fields_update', 10, 1);
+add_action('user_register', 'bs_user_profile_fields_update', 10, 1);
+
+function bs_user_profile_fields_update( $userId ) {
+
+    $user_data = get_userdata( $userId );
+    $user_role = $user_data->roles[0];
+
+    if (!current_user_can('administrator')) return;
+
+    $customer_status = $_REQUEST['bs-customer-status'];
+
+    foreach( $customer_status as $key => $value ) {
+
+        $sanitized_data[$key] = $value;
+
+    }
+    
+    update_user_meta($userId, 'bs-customer-status', $sanitized_data);
+
 }
 
 ?>
