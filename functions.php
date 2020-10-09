4e6ee6b9bc9467a7f58d4292c3537f805ed3b5d5
hudsoncliff <hudsonlciffs1999@gmail.com>
2020-10-09 16:02:46 +0900

    default main script

--- functions.php
+++ functions.php
@@ -6,13 +6,7 @@ add_action('wp_enqueue_scripts', function(){
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/style.css', ['parent-style'], date('YmdGis', filemtime(dirname(__FILE__) . '/style.css') ) );
 
-    wp_register_script( 'login-form', get_theme_file_uri( 'assets/js/login-form.js' ), [], date('YmdGis', filemtime(dirname(__FILE__) . '/assets/js/login-form.js' ) ), true );
-
-    wp_localize_script( 'login-form', 'loginForm', [
-        
-        'ajax_url' => admin_url( 'admin-ajax.php' )
-
-    ] );
+    wp_register_script( 'main-script', get_theme_file_uri( 'assets/js/main.min.js' ), [], date('YmdGis', filemtime(dirname(__FILE__) . '/assets/js/main.min.js' ) ), true );
 
 });
 
